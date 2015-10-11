b08ba5a4bd41e71725aead9c11d69c705134b4f5
SEAP UDEA <seapudea@gmail.com>
2015-10-11 10:40:03 -0500

    Commit

---
+++ util/kernels/kernels.mk
@@ -0,0 +1,16 @@
+SPICE Kernels:
+http://naif.jpl.nasa.gov/pub/naif/
+
+\begindata
+
+PATH_VALUES = ( 'data' )
+PATH_SYMBOLS = ( 'PATH' )
+KERNELS_TO_LOAD = ('$PATH/de430.bsp',
+		   '$PATH/naif0011.tls',
+		   '$PATH/pck00010.tpc',
+		   '$PATH/frames.tk',
+		   '$PATH/earth_assoc_itrf93.tf',
+		   '$PATH/earth_latest_high_prec.bpc'
+		   )
+
+\begintext
