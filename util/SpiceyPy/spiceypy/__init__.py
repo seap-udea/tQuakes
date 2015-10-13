9ce651ffbd5e607adb674122311ff82400bb6b36
SEAP UDEA <seapudea@gmail.com>
2015-10-13 03:49:25 -0500

    Commit

--- util/SpiceyPy/spiceypy/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'AndrewAnnex'
-
-__all__ = ['wrapper']
-
-from spiceypy.wrapper import *
-
-#Default setting for error reporting so that programs don't just exit out!
-erract("set", 10, "return")
-errdev("set", 10, "null")
