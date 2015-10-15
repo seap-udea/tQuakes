efc12febc0b7460cf5704e1862c87bb379a32105
SEAP UDEA <seapudea@gmail.com>
2015-10-14 23:13:15 -0500

    Commit

--- tquakes-fetch.py
+++ tquakes-fetch.py
@@ -11,6 +11,7 @@ conf=loadConf("configuration")
 # ##################################################
 station=loadConf(".stationrc")
 out=System("links -dump '%s/index.php?action=status&station_id=%s&station_status=1'"%(conf.WEBSERVER,station.station_id))
+print out
 
 # ##################################################
 # FECTH EVENTS
