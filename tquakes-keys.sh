958a581c05856f8b8b384b26599644370ae89c9f
SEAP UdeA <seapudea@gmail.com>
2015-10-17 05:12:23 -0500

    Commit

--- tquakes-keys.sh
+++ tquakes-keys.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 . configuration
-cp --no-preserve=mode,ownership stations/authorized_keys $HOMEDIR/$TQUSER/.ssh/authorized_keys
+
+echo "Installing keys..."
+cp stations/authorized_keys $HOMEDIR/$TQUSER/.ssh
+chown $TQUSER.$TQUSER $HOMEDIR/$TQUSER/.ssh/authorized_keys
+chmod og-rwx $HOMEDIR/$TQUSER/.ssh/authorized_keys
+
+echo "Receiving from all stations..."
+mysql -u $DBUSER --password=$DBPASSWORD tQuakes -e 'update Stations set station_receiving="1"'
+
+echo "Done."
