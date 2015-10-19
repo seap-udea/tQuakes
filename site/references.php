01e078d31ecc6b47d38ec2c5206589a851492c1d
SEAP <seapudea@gmail.com>
2015-10-19 17:48:17 -0500

    Commit

--- site/references.php
+++ site/references.php
@@ -1,3 +1,28 @@
 <ul>
-<li>Moncayo, G., Zuluaga, J. I., Monsalve, G. "Earthquakes and Tides in Colombia", Journal of Geosciences, 2016.</li>
+<li>Wenzel, H.G. "Earth Tide Data Processing Package ETERNA Version 3.40 Manual ETERNA34.HLP", Black Forest Observatory Universitaet Karlsruhe, 1997.</li>
+<li>Hartmann, T. & Wenzel, H.G. " The HW95 tidal potential catalogue", Geophysical Research Letters, Vol. 22, No. 24, Pages 3553-3556, 1995.</li>
+<li>Hartmann, T. & Wenzel, H.G. "The harmonic develpment of the Earth tide generating potential due to the direct effect of the planets". Geophysical Research Letters, Vol. 21, No. 18, Pages 1991-1993, 1994.</li>
+<li>Metivier, L., de Viron, O., Conrad, C.P., Renault, S., Diament, M., Patau, G. "Evidence of earthquake triggering by the solid earth tides", Earth and Planetary Science Letters 278, Pages 370-375, 2009.</li>
+<li>Vergos, G., Arabelos, D.N., Contadakis, M.E. "Evidence for tidal triggering on the earthquakes of the Hellenic Arc, Greece", Physics and Chemistry of the Earth, 2015.</li>
+<li>Wenzel, H.G. "Tide-Generating Potential for the Earth", In Wilhem, H., Zuern, W. and Wenzel, H.G. (Eds): Tidal Phenomena. Lecture Notes in Earth Sciences, 66 Springer, Berlin, 1997.</li>
+
+<li>Vidale, J.E., Agnew, D.C., Johnston, M.j., Oppenheimer, D.H. "Absence of earthquake correlation with Earth tides: An indication of high preseismic fault stress rate" Journal of Geophysical Research, Vol. 103, No. B10, Pages 24567-24572, 1998.</li>
+
+<li>Cochran, E.S., Vidale, J.E., Tanaka, S. "Earth tides can trigger shallow thrust fault earthquakes" Science Vol 306, 2004. www.sciencemag.org.</li>
+
+<li>Chen, L., Chen, J.G., Xu, Q.H. "Correlations between solid tides and worldwide earthquakes Ms>=7.0 since 199" Natural Hazards and Earth System Sciences., 12, 587-590, doi:10.514/nhess-12-587-2012.</li>
+
+<li>Beeler, N.M., Lockner, D.A. "Why earthquakes correlate waekly with the solid Earth tides: Effects of periodic stress on the rate and probabiligy of earthquake ocurrence" Journal of Geophysical Research, Vol. 108. No. B8, 2391, doi:10.1029/2001JB001518, 2013.</li>
+
+<li>Iwata, T., Katao, H. "The Correlation Between the Phase of the Moon and the Occurrences of Microearthquakes in the Tamba Region" Geophysical Research Letters, Vol. 33, L07302,doi: 10.1029/2005GL025510, 2006.</li>
+
+<li>Cochran, E.S., Vidale, J.E. "Comment on "Tidal syncronicity of the 26 December 2004 Sumatran earthquake and its aftershocks" by R.G.M.Crockett et al." Geophysical Research Letters, Vol. 34, L04302, doi: 10.1029/2006GL028639, 2007.</li>
+
+<li>Heaton, T. "Tidal Triggering of Earthquakes" Geophys. J.R. ast. Soc. 43, 307-327, 1975.</li>
+
+<li>Heaton, T. "Tidal Triggering of Earthquakes" Bulletin of the Seismological Society of America, Vol.72, No. 6, pp.2181-2200, 1982.</li>
+
+
+
+Moncayo, G., Zuluaga, J. I., Monsalve, G. "Earthquakes and Tides in Colombia", Journal of Geosciences, 2016.</li>
 </ul>
