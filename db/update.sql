91c529c56694dc6701a3542d081866cbbd9f5789
SEAP UdeA <seapudea@gmail.com>
2015-10-29 18:48:22 -0500

    Commit

--- db/update.sql
+++ db/update.sql
@@ -4,7 +4,7 @@ alter table Quakes
       add column country varchar(255),
       add column cluster1 varchar(10),
       add column cluster2 varchar(10),
-      add column cluster3 varchar(10);
+      add column cluster3 varchar(10),
       add column extra1 varchar(255),
       add column extra2 varchar(255),
       add column extra3 varchar(255),
