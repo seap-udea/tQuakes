74c3c3cc054e9a8817ef5305118a34f6a3925f2a
SEAP UDEA <seapudea@gmail.com>
2015-10-31 16:42:24 -0500

    Commit

---
+++ plots/stats/quakes-distribution.py
@@ -0,0 +1,8 @@
+# ############################################################
+# IMPORT TOOLS
+# ############################################################
+from tquakes import *
+from matplotlib import use
+use('Agg')
+import matplotlib.pyplot as plt
+execfile("%s.conf"%BASENAME)
