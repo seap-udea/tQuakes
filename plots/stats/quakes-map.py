4d5951107d698efa62fbe32bb60b994fd6dff0db
SEAP UDEA <seapudea@gmail.com>
2015-11-01 23:07:27 -0500

    Commit

---
+++ plots/stats/quakes-map.py
@@ -0,0 +1,10 @@
+# ############################################################
+# IMPORT TOOLS
+# ############################################################
+from tquakes import *
+from matplotlib import use
+use('Agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap as map
+execfile("%s.conf"%BASENAME)
+
