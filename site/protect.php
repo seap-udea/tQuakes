d19591df819bfe759365c91771e3acd37f3b4f12
SEAP UDEA <seapudea@gmail.com>
2015-11-04 11:31:23 -0500

    Commit

---
+++ site/protect.php
@@ -0,0 +1,142 @@
+<?php
+include("configuration.php");
+###############################################################
+# Page Password Protect 2.13
+###############################################################
+# Visit http://www.zubrag.com/scripts/ for updates
+############################################################### 
+##################################################################
+#  SETTINGS START
+##################################################################
+// request login? true - show login and password boxes, false - password box only
+define('USE_USERNAME', true);
+
+// User will be redirected to this page after logout
+define('LOGOUT_URL', 'index.php');
+
+// time out after NN minutes of inactivity. Set to 0 to not timeout
+define('TIMEOUT_MINUTES',180);
+
+// This parameter is only useful when TIMEOUT_MINUTES is not zero
+// true - timeout time from last activity, false - timeout time from login
+define('TIMEOUT_CHECK_ACTIVITY', true);
+
+##################################################################
+#  SETTINGS END
+##################################################################
+
+
+///////////////////////////////////////////////////////
+// do not change code below
+///////////////////////////////////////////////////////
+
+// show usage example
+if(isset($_GET['help'])) {
+  die('Include following code into every page you would like to protect, at the very beginning (first line):<br>&lt;?php include("' . str_replace('\\','\\\\',__FILE__) . '"); ?&gt;');
+}
+
+// timeout in seconds
+$timeout = (TIMEOUT_MINUTES == 0 ? 0 : time() + TIMEOUT_MINUTES * 60);
+
+// logout?
+if(isset($_GET['logout'])) {
+  setcookie("verify", '', $timeout, '/'); // clear password;
+echo<<<RESET
+<html>
+<head>
+  <META HTTP-EQUIV="refresh" CONTENT="0;URL=index.php">
+</head>
+</html>
+RESET;
+//header('Location: ' . LOGOUT_URL);
+exit();
+}
+
+if(!function_exists('showLoginPasswordProtect')) {
+
+// show login form
+function showLoginPasswordProtect($error_msg) {
+?>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Autorización</title>
+    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+  <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+</head>
+<body>
+  <style>
+    input { border: 1px solid black; }
+  </style>
+  <div style="width:500px; margin-left:auto; margin-right:auto; text-align:center">
+  <form method="post">
+    <h3>Por favor ingrese su nombre de usuario y contraseña</h3>
+    <font color="red"><?php echo $error_msg; ?></font><br />
+<?php if (USE_USERNAME) echo 'Nombre: <input type="input" name="access_name" /><br /> Usuario: <input type="input" name="access_login" /><br />Contraseña:'; ?>
+    <input type="password" name="access_password" /><p></p><input type="submit" name="Submit" value="Ingreso" />
+  </form>
+  <br />
+  <a style="font-size:9px; color: #B0B0B0; font-family: Verdana, Arial;" href="http://www.zubrag.com/scripts/password-protect.php" title="Download Password Protector">Powered by Password Protect</a>
+  </div>
+</body>
+</html>
+
+<?php
+  // stop at this point
+  die();
+}
+}
+
+// user provided password
+if (isset($_POST['access_password'])) {
+
+  $login = isset($_POST['access_login']) ? $_POST['access_login'] : '';
+  $pass = $_POST['access_password'];
+  $name = $_POST['access_name'];
+  if (!USE_USERNAME && !in_array($pass, $LOGIN_INFORMATION)
+  || (USE_USERNAME && ( !array_key_exists($login, $LOGIN_INFORMATION) || $LOGIN_INFORMATION[$login] != $pass ) ) 
+  ) {
+    showLoginPasswordProtect("Usuario o contraseña incorrecta.");
+  }
+  else {
+    // set cookie if password was validated
+    setcookie("verify", md5($login.'%'.$pass), $timeout, '/');
+    setcookie("name", $name, $timeout, '/');
+    
+    // Some programs (like Form1 Bilder) check $_POST array to see if parameters passed
+    // So need to clear password protector variables
+    unset($_POST['access_login']);
+    unset($_POST['access_password']);
+    unset($_POST['access_name']);
+    unset($_POST['Submit']);
+  }
+
+}
+
+else {
+
+  // check if password cookie is set
+  if (!isset($_COOKIE['verify'])) {
+    showLoginPasswordProtect("");
+  }
+
+  // check if cookie is good
+  $found = false;
+  foreach($LOGIN_INFORMATION as $key=>$val) {
+    $lp = (USE_USERNAME ? $key : '') .'%'.$val;
+    if ($_COOKIE['verify'] == md5($lp)) {
+      $found = true;
+      // prolong timeout
+      if (TIMEOUT_CHECK_ACTIVITY) {
+        setcookie("verify", md5($lp), $timeout, '/');
+      }
+      break;
+    }
+  }
+  if (!$found) {
+    showLoginPasswordProtect("");
+  }
+
+}
+
+?>
