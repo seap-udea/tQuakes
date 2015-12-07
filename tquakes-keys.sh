b3d1be123b3abb2ab3679078ed200d64201bb983
SEAP UdeA <seapudea@gmail.com>
2015-12-07 13:16:14 -0500

    Commit

--- tquakes-keys.sh
+++ tquakes-keys.sh
@@ -8,6 +8,6 @@ chown $TQUSER.$TQUSER $HOMEDIR/$TQUSER/.ssh/authorized_keys
 chmod og-rwx $HOMEDIR/$TQUSER/.ssh/authorized_keys
 
 echo "Receiving from all stations..."
-mysql -u $DBUSER --password=$DBPASSWORD tQuakes -e 'update Stations set station_receiving="1"'
+# mysql -u $DBUSER --password=$DBPASSWORD tQuakes -e 'update Stations set station_receiving="1"'
 
 echo "Done."
