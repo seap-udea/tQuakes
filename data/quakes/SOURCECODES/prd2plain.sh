946fad4951118f946bbc6d1480b468e36683aae6
Jorge Zuluaga <jzuluaga@urania.udea.edu.co>
2017-09-11 19:19:47 -0500

    Commit

--- data/quakes/SOURCECODES/prd2plain.sh
+++ data/quakes/SOURCECODES/prd2plain.sh
@@ -2,4 +2,5 @@
 ini=$(grep -n "77777777" $1 |cut -f 1 -d':')
 end=$(grep -n "99999999" $1 |cut -f 1 -d':')
 size=$((end-ini-1))
+#head -n $((end-1)) $1 |tail -n $size |sed -e "s/\*\*\*\*\*\*\*\*\*\*/ 0.0/"
 head -n $((end-1)) $1 |tail -n $size 
