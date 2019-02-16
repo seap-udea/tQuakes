40e54674049d2d7b41a6bda1cc227ac12e0ae8bb
SEAP <seapudea@gmail.com>
2019-02-16 12:40:08 +0000

    Commit

--- tquakes-parallel.sh
+++ tquakes-parallel.sh
@@ -14,18 +14,18 @@ touch .start
 # ALLOW GITHUB PULL
 eval $(keychain --eval id_rsa_seap)
 
+# CHECK IF STOP SIGNAL HAVE BEEN SENT
+if [ -e stop ];then 
+    echo "Stopping."
+    touch log/tquakesd-stopped
+    exit 0
+else
+    rm log/tquakesd-stopped
+fi
+
 j=0
 while [ 1 ]
 do
-    # CHECK IF STOP SIGNAL HAVE BEEN SENT
-    if [ -e stop ];then 
-	echo "Stopping."
-	touch log/tquakesd-stopped
-	exit 0
-    else
-	rm log/tquakesd-stopped
-    fi
-
     # UPDATE BRANCH
     echo "Updating repository..."
     make pull
@@ -33,9 +33,9 @@ do
     # RUN PROCESS PER EACH PROCESSOR
     for i in $(seq 1 $station_nproc)
     do
-	nsleep=$(( ( RANDOM % 10 )  + 1 ))
-	echo "Sleeping $nsleep seconds before starting..."
-	sleep $nsleep
+	#nsleep=$(( ( RANDOM % 10 )  + 1 ))
+	#echo "Sleeping $nsleep seconds before starting..."
+	#sleep $nsleep
 	python tquakes-pipeline.py &
     done
     
